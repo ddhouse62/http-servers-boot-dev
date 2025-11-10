0fb75b2d34b31f39bcd38a80f0de4dfc694a538c
Dalton House <house.dalton62@gmail.com>
2025-11-10 17:36:53 -0600

    Adds health API endpoint - lesson 1.11

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -10,8 +11,9 @@ func main() {
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
 
+	mux.HandleFunc("/healthz", handlerReadiness)
 	srv := &http.Server{
 		Handler: mux,
 		Addr:    ":" + port,
@@ -20,4 +22,19 @@ func main() {
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
 
+}
+
+func handlerReadiness(w http.ResponseWriter, req *http.Request) {
+	if req.URL.Path != "/healthz" {
+		http.NotFound(w, req)
+		return
+	}
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	n, err := w.Write([]byte(http.StatusText(http.StatusOK)))
+	if err != nil {
+		fmt.Println("Error writing response:", err)
+		return
+	}
+	log.Printf("Wrote %d bytes to client\n", n)
 }
