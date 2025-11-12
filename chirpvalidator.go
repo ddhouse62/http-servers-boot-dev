05e2fa69dae29709f97d20edc1ef52e9549abc39
ddhouse62 <house.dalton62@gmail.com>
2025-11-12 00:55:35 -0600

    Adds Profanity filter - Lesson 4.6

--- chirpvalidator.go
+++ chirpvalidator.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"slices"
+	"strings"
 )
 
 func handlerChirpValidator(w http.ResponseWriter, r *http.Request) {
@@ -13,7 +15,7 @@ func handlerChirpValidator(w http.ResponseWriter, r *http.Request) {
 	}
 
 	type returnVals struct {
-		Valid bool `json:"valid"`
+		CleanedBody string `json:"cleaned_body"`
 	}
 
 	decoder := json.NewDecoder(r.Body)
@@ -27,12 +29,30 @@ func handlerChirpValidator(w http.ResponseWriter, r *http.Request) {
 
 	const maxChirpLength = 140
 
+	cleaned_body := profanityReplacement(params.Body)
+
 	if len(params.Body) > maxChirpLength {
 		respondWithError(w, http.StatusBadRequest, "Chirp is too long", nil)
 		return
 	}
 	respondWithJSON(w, http.StatusOK, returnVals{
-		Valid: true,
+		CleanedBody: cleaned_body,
 	})
+
+}
+
+func profanityReplacement(msg string) string {
+	words := strings.Split(msg, " ")
+
+	profanity := []string{"kerfuffle", "sharbert", "fornax"}
+
+	for i := 0; i < len(words); i++ {
+		if slices.Contains(profanity, strings.ToLower(words[i])) {
+			words[i] = "****"
+		}
+	}
+
+	cleaned_words := strings.Join(words, " ")
+	return cleaned_words
 
 }
