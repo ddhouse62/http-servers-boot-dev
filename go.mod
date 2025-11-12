424a8e1e997b1eeff6324469f8adb9a4d7ad73f1
ddhouse62 <house.dalton62@gmail.com>
2025-11-12 01:49:07 -0600

    First sqlc use - Lesson 5.3

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/ddhouse62/http-servers-boot-dev
 
 go 1.25.3
+
+require github.com/google/uuid v1.6.0
+
+require github.com/lib/pq v1.10.9
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -3,16 +3,32 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"database/sql"
+
+	"github.com/ddhouse62/http-servers-boot-dev/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	godotenv.Load()
+
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Printf("Error opening sql connection: %s", err)
+	}
+
 	const filepathRoot = "."
 	const port = "8080"
 	handler := http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot)))
 
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
+		dbQueries:      database.New(db),
 	}
 
 	mux := http.NewServeMux()
@@ -26,7 +42,6 @@ func main() {
 
 	mux.HandleFunc("POST /api/validate_chirp", handlerChirpValidator)
 
-	
 	srv := &http.Server{
 		Handler: mux,
 		Addr:    ":" + port,
@@ -39,4 +54,5 @@ func main() {
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	dbQueries      *database.Queries
 }
